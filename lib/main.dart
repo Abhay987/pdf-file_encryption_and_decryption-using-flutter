c4d33785ea9e1a36495d5893f264e3fda5451502
himanshu yadav <himanshu@techtonic.asia>
2022-05-20 16:42:59 +0530

    Nothing Updated

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,8 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _isGranted = true;
   String fileName = "demoFile.pdf";
   String pdfUrl = "https://www.clickdimensions.com/links/TestPDFfile.pdf";
-  String newPdfUrl = "https://d1rz6k04k2ybb2.cloudfront.net/CengageIndia/9788131515068/pdf/YdFLSjgc3m/encrypt.pdf";
+  String newPdfUrl =
+      "https://d1rz6k04k2ybb2.cloudfront.net/CengageIndia/9788131515068/pdf/YdFLSjgc3m/encrypt.pdf";
 
   Future<Directory?> get getAppDir async {
     final appDocDir = await getExternalStorageDirectory();
@@ -49,10 +50,10 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Future<Directory> get getExternalVisibleDir async {
     var directoryData = getAppDir;
-    debugPrint("The data is -------- :  ${directoryData}");
+    debugPrint("The data is -------- :  $directoryData");
 
     if (await Directory(
-            '/storage/emulated/0/Android/data/com.example.pdf_decrypt/files')                          //  /storage/emulated/0/Android/data/com.example.pdf_decrypt/files
+            '/storage/emulated/0/Android/data/com.example.pdf_decrypt/files') //  /storage/emulated/0/Android/data/com.example.pdf_decrypt/files
         .exists()) {
       final externalDir = Directory(
           '/storage/emulated/0/Android/data/com.example.pdf_decrypt/files');
@@ -96,42 +97,43 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<List<int>> _decryptData(encData) async {
     debugPrint("File decryption in progress ... ");
     enc.Encrypted encrypted = enc.Encrypted(encData);
-    return MyEncrypt.myCryptor.decryptBytes(encrypted,iv: MyEncrypt.myIv);
+    return MyEncrypt.myCryptor.decryptBytes(encrypted, iv: MyEncrypt.myIv);
   }
 
   _encryptData(plainString) {
     debugPrint("Encrypting file");
     final encrypted =
-        MyEncrypt.myCryptor.encryptBytes(plainString,iv: MyEncrypt.myIv);
+        MyEncrypt.myCryptor.encryptBytes(plainString, iv: MyEncrypt.myIv);
     debugPrint("----------------------  1     --------------------");
     debugPrint("Encrypted key is :  ${MyEncrypt.myKey.base64}");
     debugPrint("Encrypted IV is :  ${MyEncrypt.myIv.base64}");
     debugPrint("Encrypted encryptor is :  ${MyEncrypt.myCryptor}");
     return encrypted.bytes;
   }
-    _getNewNormalFile(String path,Directory d, filename) async{
-      if (await canLaunchUrlString(path)) {
-        debugPrint("Data Downloading ...");
-        var resp = await http.get(Uri.parse(path));
+
+  _getNewNormalFile(String path, Directory d, filename) async {
+    if (await canLaunchUrlString(path)) {
+      debugPrint("Data Downloading ...");
+      var resp = await http.get(Uri.parse(path));
       /*  var encResult = resp.bodyBytes;
         String p = await _writeData(encResult, d.path + '/$filename.aes');
         debugPrint("file download  successfully : $p");
         Uint8List encData = await _readData(d.path + '/$filename.aes');           */
 
-        var plainData = await _decryptData(resp.bodyBytes);
-        String pathNew = await _writeData(plainData, d.path + '/$filename');
-        // await File(d.path + '/$filename.aes').delete();
-        debugPrint("File decrypted successfully : $pathNew");
-
-      } else {
-        debugPrint("Can't Launch Url. ");
-      }
+      var plainData = await _decryptData(resp.bodyBytes);
+      String pathNew = await _writeData(plainData, d.path + '/$filename');
+      // await File(d.path + '/$filename.aes').delete();
+      debugPrint("File decrypted successfully : $pathNew");
+    } else {
+      debugPrint("Can't Launch Url. ");
     }
+  }
+
   _getNormalFile(Directory d, filename) async {
     Uint8List encData = await _readData(d.path + '/$filename.aes');
     var plainData = await _decryptData(encData);
     String p = await _writeData(plainData, d.path + '/$filename');
-   // await File(d.path + '/$filename.aes').delete();
+    // await File(d.path + '/$filename.aes').delete();
     debugPrint("File decrypted successfully : $p");
   }
 
@@ -170,7 +172,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 onPressed: () async {
                   if (_isGranted) {
                     Directory d = await getExternalVisibleDir;
-                   _getNewNormalFile(newPdfUrl, d, fileName);
+                    _getNewNormalFile(newPdfUrl, d, fileName);
                   } else {
                     debugPrint("No Permission granted");
                     requestStoragePermission();
@@ -200,5 +202,5 @@ class MyEncrypt {
   // enc.Key.fromSecureRandom(16); //   enc.Key.fromUtf8('263BC60258FF4876');
   static final myIv = enc.IV.fromBase16('d981d69128e02dd0984c61a8e88f4d13');
   // enc.IV.fromSecureRandom(16);  //  enc.IV.fromUtf8('KartikSignal987');
-  static final myCryptor = enc.Encrypter(enc.AES(myKey,mode: enc.AESMode.cbc));
+  static final myCryptor = enc.Encrypter(enc.AES(myKey, mode: enc.AESMode.cbc));
 }
